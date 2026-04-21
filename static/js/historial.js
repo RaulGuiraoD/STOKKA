27f8e4d616d103766adc7c40b68c3c397ff010a4
Raul <raulguiraodiaz@gmail.com>
2026-04-21 15:27:46 +0200

    Refactorizacion minima

--- static/js/historial.js
+++ static/js/historial.js
@@ -1,20 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const searchInput = document.getElementById('filtro-historial');
-    const tableRows = document.querySelectorAll('#tabla-historial tbody tr');
-
-    if (searchInput) {
-        searchInput.addEventListener('input', function (e) {
-            const query = e.target.value.toLowerCase();
-
-            tableRows.forEach(row => {
-                // Buscamos en toda la fila para que sea flexible (producto o usuario)
-                const text = row.innerText.toLowerCase();
-                row.style.display = text.includes(query) ? '' : 'none';
-            });
-        });
-    }
-
-    /* CALENDARIO */
     const inputSearch = document.getElementById('filtro-historial');
     const inputDate = document.getElementById('busqueda-fecha');
     const selectTipo = document.getElementById('filtro-tipo');
