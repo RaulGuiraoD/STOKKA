c9af37862c0bb292870d2277cd35b1eb82f52437
Raul <raulguiraodiaz@gmail.com>
2026-05-04 12:22:00 +0200

    Funciones de js empresa añadidas a su js

--- static/js/general.js
+++ static/js/general.js
@@ -273,37 +273,3 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
-
-// BOTON CONFIRMAR ELIMINACIÓN DE LA EMPRESA + MODALES
-document.addEventListener('DOMContentLoaded', function () {
-    const inputNombre = document.getElementById('inputConfirmacionEmpresa');
-    const btnSiguiente = document.getElementById('btnSiguienteEliminar');
-    const btnIrAlUltimo = document.getElementById('btnIrAlUltimoPaso');
-    const inputHidden = document.getElementById('inputHiddenNombre');
-
-    // Instancias de los modales
-    const m1 = new bootstrap.Modal(document.getElementById('modalEliminarEmpresa'));
-    const m2 = new bootstrap.Modal(document.getElementById('modalConfirmacionFinal'));
-    const m3 = new bootstrap.Modal(document.getElementById('modalUltimoAviso'));
-
-    // 1. Validar nombre en Modal 1
-    inputNombre.addEventListener('input', function () {
-        const nombreReal = this.getAttribute('data-nombre').trim();
-        const match = this.value.trim() === nombreReal;
-        btnSiguiente.disabled = !match;
-        btnSiguiente.style.opacity = match ? '1' : '0.5';
-    });
-
-    // 2. De Modal 1 a Modal 2
-    btnSiguiente.addEventListener('click', function() {
-        inputHidden.value = inputNombre.value; // Guardamos el nombre para el form final
-        m1.hide();
-        setTimeout(() => m2.show(), 400); // Pequeño delay para suavidad
-    });
-
-    // 3. De Modal 2 a Modal 3
-    btnIrAlUltimo.addEventListener('click', function() {
-        m2.hide();
-        setTimeout(() => m3.show(), 400);
-    });
-});
